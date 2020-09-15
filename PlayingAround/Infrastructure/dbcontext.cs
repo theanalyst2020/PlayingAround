1ddf15b97d6b4a94c9981c470658e16ca9893553
theanalyst2020 <theanalyst2020@hotmail.com>
2020-09-15 18:49:57 +0400

    added dbcontext file and imported tools and sql frameworks

---
+++ PlayingAround/Infrastructure/dbcontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayingAround.Infrastructure
+{
+    public class dbcontext : DbContext
+    {
+        public dbcontext(DbContextOptions<dbcontext> options):base(options)
+        {
+
+        }
+        public DbSet<Page> Pages { get; set; }
+    }
+}
--- PlayingAround/Models/Page.cs
+++ PlayingAround/Models/Page.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 namespace PlayingAround.Models
 {
     public class Page
-    {//
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+        public int Sorting { get; set; }
     }
 }
